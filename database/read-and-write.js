4dcf98f2909a206afa8bd7f9874103cf8e3dcf20
Alvin80Bcn <92582938+Alvin80Bcn@users.noreply.github.com>
2024-04-19 06:01:53 +0200

    Initialize ss-auth-snippets

---
+++ database/read-and-write.js
@@ -0,0 +1,31 @@
+// These samples are intended for Web so this import would normally be
+// done in HTML however using modules here is more convenient for
+// ensuring sample correctness offline.
+import firebase from "firebase/app";
+import "firebase/database";
+
+// [START rtdb_write_new_user]
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+// [END rtdb_write_new_user]
+
+function writeUserDataWithCompletion(userId, name, email, imageUrl) {
+  // [START rtdb_write_new_user_completion]
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  }, function(error) {
+    if (error) {
+      // The write failed...
+    } else {
+      // Data saved successfully!
+    }
+  });
+  // [START rtdb_write_new_user_completion]
+}
